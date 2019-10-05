8401b17719ce9247a11a32611d5b2181296201b0
KMikalsen <KMikalsen@users.noreply.github.com>
2019-10-05 10:54:11 +0200

    add moment.js

--- ChromeExtension/content_script.js
+++ ChromeExtension/content_script.js
@@ -1,6 +1,14 @@
 function findGameTimes() {
     let gameTimes = document.querySelectorAll('.card-time')
-    console.log(gameTimes)
+    for (let times of gameTimes){
+        times.innerText = moment().format('MMMM Do YYYY, h:mm:ss a')
+    }
 }
 
-findGameTimes()
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('moment.min.js');
+s.onload = function() {
+    this.remove();
+    findGameTimes()
+};
+(document.head || document.documentElement).appendChild(s);
